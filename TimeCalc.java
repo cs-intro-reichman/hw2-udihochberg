4a503fbbaa6a6f3e71015715d12f17480e2f16a5
udi hochberg <udi.hochberg@post.runi.ac.il>
2024-11-13 08:51:04 +0200

    init commit

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,29 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+        
+        int totalMinutes = (hours * 60) + minutes + minutesToAdd;
+        int totalHours = totalMinutes / 60;
+        int newHour = totalMinutes % 24;
+        int newMinutes = totalMinutes - (totalHours * 60);
+        if (newHour < 10)
+        {
+            System.out.print("0"+newHour+":");
+        } 
+        else 
+        {
+            System.out.print(newHour+":");
+        }
+        if (newMinutes < 10)
+        {
+            System.out.print("0"+newMinutes);
+        }
+        else
+        {
+            System.out.print(newMinutes);
+        }
     }
 }
